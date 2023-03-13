a5d960e84d26884bba30e5b7f978bb535ed9e210
Morumbi <zun0323@gmail.com>
2023-03-13 18:14:03 -0300

    #5.14 Stream detail

---
+++ src/pages/live/[id].tsx
@@ -0,0 +1,84 @@
+import type { NextPage } from 'next';
+
+const LiveDetail: NextPage = () => {
+  return (
+    <div className=' px-4 pt-14'>
+      <div>
+        <div className='aspect-video w-full rounded-md bg-slate-300'></div>
+        <h1 className='mt-4 text-2xl font-bold text-gray-900'>
+          Let's try potato
+        </h1>
+      </div>
+      <div className='mt-3 h-[45vh] overflow-y-scroll '>
+        <div className='mt-6 flex items-start space-x-3 px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            Hi how much are you selling them for?
+          </span>
+        </div>
+        <div className='mt-6 flex flex-row-reverse items-start space-x-3 space-x-reverse px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            I wnat $20,000
+          </span>
+        </div>
+        <div className='mt-6 flex items-start space-x-3 px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            Hi how much are you selling them for?
+          </span>
+        </div>
+        <div className='mt-6 flex flex-row-reverse items-start space-x-3 space-x-reverse px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            I wnat $20,000
+          </span>
+        </div>
+        <div className='mt-6 flex items-start space-x-3 px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            Hi how much are you selling them for?
+          </span>
+        </div>
+        <div className='mt-6 flex flex-row-reverse items-start space-x-3 space-x-reverse px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            I wnat $20,000
+          </span>
+        </div>
+        <div className='mt-6 flex items-start space-x-3 px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            Hi how much are you selling them for?
+          </span>
+        </div>
+        <div className='mt-6 flex flex-row-reverse items-start space-x-3 space-x-reverse px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            I wnat $20,000
+          </span>
+        </div>
+        <div className='mt-6 flex items-start space-x-3 px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            Hi how much are you selling them for?
+          </span>
+        </div>
+        <div className='mt-6 flex flex-row-reverse items-start space-x-3 space-x-reverse px-3'>
+          <div className='aspect-square w-10 rounded-full bg-slate-300'></div>
+          <span className='w-1/2 rounded-md border border-gray-300 to-gray-700 p-2 text-sm'>
+            I wnat $20,000
+          </span>
+        </div>
+      </div>
+      <div className='relative flex items-center justify-center pt-4'>
+        <input className='w-full rounded-full' type='text' />
+        <button className='absolute right-0 mr-2 flex items-center justify-center rounded-full bg-orange-400 px-3 py-1 font-medium text-white ring-orange-500 ring-offset-2 hover:bg-orange-500 hover:text-white focus:ring-2'>
+          &rarr;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LiveDetail;
