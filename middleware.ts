c935dd6b735d1bcbf2658c7fd46569e70a18780d
Morumbi <zun0323@gmail.com>
2023-04-05 17:13:29 -0300

    back middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, userAgent } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  // const ua = userAgent(req);
+  // if (ua?.isBot) {
+  //   return new Response("Plz don't be a bot. be human", {
+  //     status: 403,
+  //     statusText: 'Nope!!',
+  //   });
+  // }
+
+  if (!req.cookies.has('carrotsession') && !req.url.includes('/enter')) {
+    const url = req.nextUrl.clone();
+    console.log(url);
+    url.pathname = '/enter';
+    console.log(url);
+    return NextResponse.redirect(url);
+  }
+}
+export const config = {
+  matcher: ['/((?!api|_next/static|favicon.ico).*)'],
+};
